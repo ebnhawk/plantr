72499125bc79ebcd0297c82bdfb4dfd40075ed68
Molly Hanes <mollyfhanes@gmail.com>
2018-03-12 16:47:16 -0500

    UGH

--- models.js
+++ models.js
@@ -1,32 +1,45 @@
 const sequelize = require('sequelize');
-const db = new sequelize('postgres://localhost:5432/plantr', {logging: true});
+const db = new sequelize('postgres://localhost:5432/plantr', {
+	logging: false
+});
 
 // ----------------------------------------
-const Gardener = db.define('Gardener', {
-    name: {
-        type: sequelize.STRING}, 
-    age: {
-        type: sequelize.INTEGER}});
+const Gardener = db.define('gardeners', {
+	name: {
+		type: sequelize.STRING
+	},
+	age: {
+		type: sequelize.INTEGER
+	}
+});
 // ----------------------------------------
 
-const Plot = db.define('Plot', {
-    size: {
-        type: sequelize.INTEGER}, 
-    shaded: {
-        type: sequelize.BOOLEAN}});
+const Plot = db.define('plots', {
+	size: {
+		type: sequelize.INTEGER
+	},
+	shaded: {
+		type: sequelize.BOOLEAN
+	}
+});
 // ----------------------------------------
-        
-const Vegetable = db.define('Vegetable', {
-    name: {
-        type: sequelize.STRING}, 
-    color: {
-        type: sequelize.STRING},
-    planted_on: {
-        type: sequelize.DATEONLY}});
+
+const Vegetable = db.define('vegetables', {
+	name: {
+		type: sequelize.STRING
+	},
+	color: {
+		type: sequelize.STRING
+	},
+	planted_on: {
+		type: sequelize.DATEONLY
+	}
+});
 // ----------------------------------------
 
 Plot.belongsTo(Gardener);
-Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
-Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 
-module.exports = db;
+module.exports = { db, Gardener, Plot, Vegetable };
--- seed.js
+++ seed.js
@@ -1,12 +1,25 @@
-const db = require('models.js');
+const { db, Gardener, Plot, Vegetable } = require('./models.js');
 
-db.sync({force: true})
-  .then(() => {
-    console.log('Database synced!')
-    db.close()
-  })
-  .catch(err => {
-    console.log('Disaster! Something went wrong! ')
-    console.log(err)
-    db.close()
-  });
+db.sync({ force: true }).then(() => {
+	const tomato = Vegetable.build({
+		name: 'tomato',
+		color: 'red',
+		planted_on: new Date('2018-03-12')
+	});
+	tomato.save().catch(error => console.log(error));
+});
+
+db
+	.sync({ force: true })
+	.then(() => {
+		return db.drop();
+	})
+	.then(() => {
+		console.log('Database synced!');
+		db.close();
+	})
+	.catch(err => {
+		console.log('Disaster! Something went wrong! ');
+		console.log(err);
+		db.close();
+	});
